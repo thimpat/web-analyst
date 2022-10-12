636b6998178a6664796762316fca8471d823a30a
Patrice THIMOTHEE <thimpat@gmail.com>
2022-10-12 16:50:48 +0100

    REFACTOR: Update view

--- web/js/start.mjs
+++ web/js/start.mjs
@@ -1,115 +1,53 @@
-import {generateBarChart, generateDataTables, generateLineChart} from "./chart-generator.mjs";
+import {
+    generateBarChart,
+    generateDataTables,
+    generatePieChart,
+    getData
+} from "./chart-generator.mjs";
+import {MEANINGFUL_DATA_FILES} from "./constants.mjs";
+import {
+    buildBrowserPopularityPie,
+    buildDataTable,
+    buildLanguagePie, buildVisitorDay,
+    buildVisitorsYear,
+    buildVisitorWeek
+} from "./elt-views.mjs";
 
-const init = () =>
+const init = async () =>
 {
     try
     {
-        const $lineChart = document.getElementById("myChart1");
-
-        const options = {
-            responsive: true,
-            plugins   : {
-                legend: {
-                    position: "top",
-                },
-                title : {
-                    display: true,
-                    text   : "Chart.js Bar Chart"
-                }
-            }
-        };
-
-        const data = [0, 10, 5, 2, 20, 30, 45];
-
         // generateLineChart($lineChart, {title: "My First dataset", labels, data, options});
 
         // ------------------------------------------------
         // Visitor per day
         // ------------------------------------------------
-        const labelHours = [];
-        for (let i = 0; i < 24; ++i)
-        {
-            labelHours.push(i + "h");
-        }
-
-        const $barChart1 = document.getElementById("visitorHours");
-        const options1 = {
-            responsive: true,
-            plugins   : {
-                legend: {
-                    position: "top",
-                },
-                title : {
-                    display: true,
-                    text   : "Today"
-                }
-            }
-        };
-        generateBarChart($barChart1, {title: "Visitor per hour", labels: labelHours, data, options: options1});
+        buildVisitorDay();
 
         // ------------------------------------------------
         // Visitor per week
         // ------------------------------------------------
-        const labelWeek = [
-            "Monday",
-            "Tuesday",
-            "Wednesday",
-            "Thursday",
-            "Friday",
-            "Saturday",
-            "Sunday",
-        ];
-
-        const $barChart2 = document.getElementById("visitorsWeek");
-        const options2 = {
-            responsive: true,
-            plugins   : {
-                legend: {
-                    position: "top",
-                },
-                title : {
-                    display: true,
-                    text   : "This week"
-                }
-            }
-        };
-        generateBarChart($barChart2, {title: "Visitor per day", labels: labelWeek, data, options: options2});
+        buildVisitorWeek();
 
         // ------------------------------------------------
         // Visitor per month
         // ------------------------------------------------
-        const labelsMonths = [
-            "January",
-            "February",
-            "March",
-            "April",
-            "May",
-            "June",
-            "July",
-            "August",
-            "September",
-            "October",
-            "November",
-            "December",
-        ];
+        buildVisitorsYear();
 
-        const $barChart3 = document.getElementById("visitorsYear");
-        const options3 = {
-            responsive: true,
-            plugins   : {
-                legend: {
-                    position: "top",
-                },
-                title : {
-                    display: true,
-                    text   : "This year"
-                }
-            }
-        };
-        generateBarChart($barChart3, {title: "Visitor per month", labels: labelsMonths, data, options: options3});
+        // ------------------------------------------------
+        // Browsers
+        // ------------------------------------------------
+        buildBrowserPopularityPie();
 
-        // Data Tables
-        generateDataTables();
+        // ------------------------------------------------
+        // Language
+        // ------------------------------------------------
+        buildLanguagePie();
+
+        // ------------------------------------------------
+        // All data
+        // ------------------------------------------------
+        await buildDataTable();
 
         return true;
     }
@@ -126,7 +64,7 @@ const init = () =>
 {
     try
     {
-        init();
+        await init();
 
         return true;
     }
