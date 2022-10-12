4f3b142debc47062e2bb45df8536f8b20771124e
Patrice THIMOTHEE <thimpat@gmail.com>
2022-10-12 16:49:31 +0100

    REFACTOR: Refactor data builder

---
+++ constants.cjs
@@ -0,0 +1,14 @@
+
+const MEANINGFUL_LOG_FILES = {
+    HITS_LOG_PATH: "hits.log",
+    IPS_LOG_PATH: "ips.log",
+};
+
+const MEANINGFUL_DATA_FILES = {
+    HITS_DATA_PATH: "hits.json",
+    TODAY_DATA_PATH: "today.json"
+};
+
+
+module.exports.MEANINGFUL_LOG_FILES = MEANINGFUL_LOG_FILES;
+module.exports.MEANINGFUL_DATA_FILES = MEANINGFUL_DATA_FILES;
