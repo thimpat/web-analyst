057a3067914ad9384eb51bfd9bdeb558f55615a3
Patrice THIMOTHEE <thimpat@gmail.com>
2022-10-18 13:24:43 +0100

    WIP

---
+++ .eslintrc.cjs
@@ -0,0 +1,25 @@
+module.exports = {
+    env: {
+        browser: true,
+        node: true
+    },
+    parserOptions: {
+        "sourceType": "module",
+        "ecmaVersion": 2022
+    },
+    rules: {
+        "comma-dangle": 0,
+        "comma-style": ["error", "last"],
+        "no-bitwise": "off",
+        "no-console": "off",
+        "no-nested-ternary": "off",
+        "no-plusplus": "off",
+        "prefer-const": "off",
+        "spaced-comment": "off",
+        "no-debugger": "error",
+        "quotes": ["error", "double", { "avoidEscape": true, "allowTemplateLiterals": true }],
+        "semi": 2,
+        "no-unused-vars": "error",
+        "max-len": ["error", {code: 200}]
+    }
+};
