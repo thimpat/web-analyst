5cb8897441e6f7d1d33959769f10722a2bd402ea
Patrice THIMOTHEE <thimpat@gmail.com>
2022-12-23 23:31:19 +0000

    CI: Build web directory from template folder

---
+++ tools/templator.cjs
@@ -0,0 +1,60 @@
+const {existsSync, readFileSync, writeFileSync, mkdirSync} = require("fs");
+
+const shell = require("shelljs");
+const glob = require("glob");
+
+const {joinPath} = require("@thimpat/libutils");
+
+
+const rootDir = joinPath(__dirname, "..");
+process.chdir(rootDir);
+shell.cd(rootDir);
+
+const packageJsonPath = joinPath(rootDir, "package.json");
+const packageJson = require(packageJsonPath);
+
+const templatesDir = joinPath(rootDir, "templates");
+mkdirSync(templatesDir, {recursive: true});
+
+const outputDir = joinPath(rootDir, "web");
+mkdirSync(outputDir, {recursive: true});
+
+// options is optional
+const files = glob.sync(`**/*`, {
+    nodir : true,
+    cwd   : templatesDir
+});
+
+for (let i = 0; i < files.length; ++i)
+{
+    const originalFile = joinPath(templatesDir, files[i]);
+    const targetFile = joinPath(outputDir, files[i]);
+
+    const initialContent = readFileSync(originalFile, {encoding: "utf-8"});
+
+    const finalContent = initialContent.replace(/\{\{([^}]+)}}/g, (match, offset) =>
+    {
+        const val = packageJson[offset];
+        if (!val)
+        {
+            return match;
+        }
+        return val;
+    });
+
+    if (initialContent === finalContent)
+    {
+        if (!existsSync(targetFile))
+        {
+            writeFileSync(targetFile, finalContent, {encoding: "utf8"});
+            console.log(`Generated: [${targetFile}]`);
+        }
+    }
+    else
+    {
+        writeFileSync(targetFile, finalContent, {encoding: "utf8"});
+        console.log(`Updated: [${originalFile}]`);
+    }
+
+}
+
